17dd5e35cf552a978b1a43f3561dbad46c2d266a
Thomas Fraunholz <tf@sourcemaker.org>
2024-01-17 16:33:57 +0100

    capture the error in pip install

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-pip install -r requirements.txt -f https://download.pytorch.org/whl/cu118
+pip install -r requirements.txt -f https://download.pytorch.org/whl/cu118 || true
 
 exec "$@"
